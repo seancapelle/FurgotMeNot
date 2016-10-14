b2fcb55f8dd7187a00ee2c68ee65699fb60a48ad
RisinPhoenix <DavidBunner@gmail.com>
2016-10-13 22:31:05 -0400

    image upload

--- controllers/adopt.js
+++ controllers/adopt.js
@@ -1,9 +1,75 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
-
+var multer = require('multer');
 router.get('/', function(req, res) {
   res.render('adopt');
 });
 
+
 module.exports = router;
+
+// router.post('/create', function(req,res) {
+// 	console.log('/create hit');
+// 	console.log(req.body);
+// 	models.Users.findAll({
+//     where: {email_address: req.body.email_address}
+//   }).then(function(users) {
+//   		console.log(users);
+//   		console.log('.then');
+  		
+// 		if (users.length > 0){
+// 			console.log(users);
+// 			console.log('hit if');
+// 			res.send('we already have an email or username for this account')
+// 		}else{
+// 			console.log('else');
+// 			// Solution:
+// 			// =========
+
+// 			// Using bcrypt, generate a 10-round salt,
+// 			// then use that salt to hash the user's password.
+// 			bcrypt.genSalt(10, function(err, salt) {
+// 					bcrypt.hash(req.body.password, salt, function(err, hash) {
+// 						console.log(req.body.email_address);
+// 						// Using the User model, create a new user,
+// 						// storing the email they sent and the hash you just made
+// 						models.Users.create({
+// 							users_name: req.body.users_name,
+// 							email_address: req.body.email_address,
+// 							password: hash, age: req.body.age,
+// 							address: req.body.address,
+// 							phone: req.body.phone,
+// 							location: req.body.location,
+// 							zipcode: req.body.zip
+// 						})
+// 						// In a .then promise connected to that create method,
+// 						// save the user's information to req.session
+// 						// as shown in these comments
+// 						.then(function(users){
+
+
+// 							// so what's happening here?
+// 							// we enter the user's session by setting properties to req.
+
+// 							// we save the logged in status to the session
+// 		          req.session.logged_in = true;
+// 		          console.log(req.session.logged_in);
+// 		          // the username to the session
+// 					req.session.users_name = users.users_name;
+// 					console.log(users.users_name);
+// 					// the user id to the session
+// 		          req.session.users_id = users.id;
+// 		          console.log(users.id);
+// 		          // and the user's email.
+// 		          req.session.users_email = users.email_address;
+// 		          console.log (users.email_address);
+// 		          // redirect to home on login
+// 							res.redirect('/')
+// 						});
+// 					});
+// 			});
+
+// 		}
+// 	});
+// });
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var session = require('express-session');
 var methodOverride = require('method-override'); // for deletes in express
 var debug = require('debug')('express-example');
 var models = require("./models");
-
+var multer = require('multer');
 
 // Our model controllers (rather than routes)
 var homepage_controller = require('./controllers/homepage');
@@ -22,6 +22,30 @@ var login_controller = require('./controllers/login');
 
 // instantiate our app
 var app = express();
+
+// express use app multer
+// set params for multer
+app.use(multer({
+    dest: './',
+    limits: {
+        fieldNameSize: 50,
+        files: 5,
+        fields: 3,
+        fileSize: (1024 * 1024) * 100
+    },
+    //gives a unique file name
+    rename: function(fieldname, filename) {
+        return filename;
+    },
+    //gives a console.log for uploading
+    onFileUploadStart: function(file) {
+        console.log('Starting file upload process.');
+        if (file.mimetype !== 'text/img') {
+            return false;
+        }
+    },
+    inMemory: true
+}).any());
 
 // override POST to have DELETE and PUT
 app.use(methodOverride('_method'))
@@ -60,21 +84,37 @@ app.use('/adopt', adopt_controller);
 app.use('/login', login_controller);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+app.post('/image', function(req, res){
+  console.log('/image hit');
+  console.log(req.body) // form fields
+  console.log(req.file) // form files
+
+    // var file = req.file.path
+
+  // for the length of files write to database
+
+  // models.images.create({
+
+  // })
+})
+
+
 
 // error handler
 // no stacktraces leaked to user unless in development environment
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: (app.get('env') === 'development') ? err : {}
-  });
-});
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: (app.get('env') === 'development') ? err : {}
+//   });
+// });
 
 
 //set port
